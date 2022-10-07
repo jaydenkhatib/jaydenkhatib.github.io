85d28f4662e30301625123b787be0bfaab4a7f63
jaydenkhatib <76129878+jaydenkhatib@users.noreply.github.com>
2022-10-07 19:09:47 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,26 +1,26 @@
 // init Isotope
 var $grid = $('.portfolio').isotope({
     itemSelector: '.works',
-    layoutMode: 'fitRows'
-  });
+    layoutMode: 'fitRows',
+    getSortData: {
+      category: '[data-category]'
+    }
+    }
+    );
   
-  // filter functions
-
   // bind filter button click
   $('#filters').on( 'click', 'a', function() {
     var filterValue = $( this ).attr('data-filter');
     // use filterFn if matches value
-    filterValue = filterFns[ filterValue ] || filterValue;
+    filterValue = filterValue;
     $grid.isotope({ filter: filterValue });
   });
   
   // change is-checked class on buttons
-  $('.filters').each( function( i, filters ) {
+  $('#filters').each( function( i, filters ) {
     var $filters = $( filters );
-    $filters.on( 'click', 'button', function() {
+    $filters.on( 'click', 'a', function() {
       $filters.find('.is-checked').removeClass('is-checked');
       $( this ).addClass('is-checked');
     });
   });
-    
-  
