fe65dccb6c721468b60f894d7bd314a848577084
jaydenkhatib <76129878+jaydenkhatib@users.noreply.github.com>
2022-11-03 00:38:09 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+var $grid = $('.grid').isotope({
+  itemSelector: '.element-item';
+});
+// bind filter button click
+$('.filters-button-group').on( 'click', 'button', function() {
+  var filterValue = $( this ).attr('data-filter');
+  console.log(filterValue);
+  $grid.isotope({ filter: filterValue });
+});
+// change is-checked class on buttons
+$('.button-group').each( function( i, buttonGroup ) {
+  var $buttonGroup = $( buttonGroup );
+  $buttonGroup.on( 'click', 'button', function() {
+    $buttonGroup.find('.is-checked').removeClass('is-checked');
+    $( this ).addClass('is-checked');
+  });
+});
+
 $(document).ready(function(){
   
   var mousePos = {};
@@ -35,25 +53,4 @@ $(document).ready(function(){
     }
   }, 1);
   draw();
-});
-
-// init Isotope
-
-var $grid = $('.grid').isotope({
-  itemSelector: '.element-item',
-  layoutMode: 'fitRows'
-});
-// bind filter button click
-$('.filters-button-group').on( 'click', 'button', function() {
-  var filterValue = $( this ).attr('data-filter');
-  console.log(filterValue);
-  $grid.isotope({ filter: filterValue });
-});
-// change is-checked class on buttons
-$('.button-group').each( function( i, buttonGroup ) {
-  var $buttonGroup = $( buttonGroup );
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $( this ).addClass('is-checked');
-  });
 });
