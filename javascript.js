6b016ca81f34e36f675bbef3a24c85750038fcf4
jaydenkhatib <76129878+jaydenkhatib@users.noreply.github.com>
2022-11-03 00:56:13 -0400

    update

--- javascript.js
+++ javascript.js
@@ -1,24 +1,3 @@
-var $grid = $('#portfolio').isotope({
-  itemSelector: '.element-item'
-});
-console.log($grid);
-
-// bind filter button click
-$('.filters-button-group').on('click', 'button', function() {
-  var filterValue = $(this).attr('data-filter');
-  $grid.isotope({filter: filterValue});
-  console.log(filterValue);
-});
-
-// change is-checked class on buttons
-$('.button-group').each(function(i, buttonGroup) {
-  var $buttonGroup = $(buttonGroup);
-  $buttonGroup.on( 'click', 'button', function() {
-    $buttonGroup.find('.is-checked').removeClass('is-checked');
-    $(this).addClass('is-checked');
-  });
-});
-
 $(document).ready(function(){
   
   var mousePos = {};
