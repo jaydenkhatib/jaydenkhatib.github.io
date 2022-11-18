af627dda3d74b912f71d9cdce77d626c5409af68
jaydenkhatib <76129878+jaydenkhatib@users.noreply.github.com>
2022-11-18 14:08:21 -0500

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,3 +1,42 @@
+$(document).ready(function(){
+  
+  var mousePos = {};
+
+ function getRandomInt(min, max) {
+   return Math.round(Math.random() * (max - min + 1)) + min;
+ }
+  
+  $(window).mousemove(function(e) {
+    mousePos.x = e.pageX;
+    mousePos.y = e.pageY;
+  });
+  
+  $(window).mouseleave(function(e) {
+    mousePos.x = -1;
+    mousePos.y = -1;
+  });
+  
+  var draw = setInterval(function(){
+    if(mousePos.x > 0 && mousePos.y > 0){
+      
+      var range = 15;
+      
+      var color = "background: rgb("+getRandomInt(0,200)+","+getRandomInt(0,200)+","+getRandomInt(190,255)+");";
+      
+      var sizeInt = getRandomInt(10, 30);
+      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
+      
+      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
+      
+      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
+
+      var style = left+top+color+size;
+      $("<div class='ball' style='" + style + "'></div>").appendTo('body').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
+    }
+  }, 1);
+  draw();
+});
+
 window.onload = () => {
   filterSelection('all')
 };
@@ -40,8 +79,8 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("btn_container");
-var btns = btnContainer.getElementsByClassName("btn");
+var btn_container = document.getElementById("btn_container");
+var btns = btn_container.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
@@ -49,42 +88,3 @@ for (var i = 0; i < btns.length; i++) {
     this.className += " active";
   });
 }
-
-$(document).ready(function(){
-  
-  var mousePos = {};
-
- function getRandomInt(min, max) {
-   return Math.round(Math.random() * (max - min + 1)) + min;
- }
-  
-  $(window).mousemove(function(e) {
-    mousePos.x = e.pageX;
-    mousePos.y = e.pageY;
-  });
-  
-  $(window).mouseleave(function(e) {
-    mousePos.x = -1;
-    mousePos.y = -1;
-  });
-  
-  var draw = setInterval(function(){
-    if(mousePos.x > 0 && mousePos.y > 0){
-      
-      var range = 15;
-      
-      var color = "background: rgb("+getRandomInt(0,200)+","+getRandomInt(0,200)+","+getRandomInt(190,255)+");";
-      
-      var sizeInt = getRandomInt(10, 30);
-      size = "height: " + sizeInt + "px; width: " + sizeInt + "px;";
-      
-      var left = "left: " + getRandomInt(mousePos.x-range-sizeInt, mousePos.x+range) + "px;";
-      
-      var top = "top: " + getRandomInt(mousePos.y-range-sizeInt, mousePos.y+range) + "px;"; 
-
-      var style = left+top+color+size;
-      $("<div class='ball' style='" + style + "'></div>").appendTo('body').one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function(){$(this).remove();}); 
-    }
-  }, 1);
-  draw();
-});
